217f2a8cff273e4b9b60c8ef83f9eab4e7c6a6c3
krueger80 <mathieu.laflamme@gmail.com>
2026-02-10 00:50:26 -0500

    fix: updates .gitignore to correctly exclude generated environment files

--- client/src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  debug: true,
-  supabase: {
-    url: 'https://ymlzucwvdwnhndooctmb.supabase.co',
-    key: 'sb_publishable_JKEaZCcP81MOe-VpLnOUYA_RF1eFq2k',
-    anonKey: 'sb_publishable_JKEaZCcP81MOe-VpLnOUYA_RF1eFq2k' // Fallback/Legacy support if needed
-  }
-};
